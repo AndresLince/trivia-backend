9270fad19ef192bac9700837ac217acb57b608a1
AndresLince <lynxdarmatron@gmail.com>
2023-02-28 07:58:28 -0500

    Add auth handler structure

---
+++ src/api/handlers/auth.handler.ts
@@ -0,0 +1,9 @@
+import { Express, Request, Response } from 'express';
+
+export class AuthHandler {
+    signUp(request: Request, response: Response): void {
+        response.status(200).send({
+            message: "Usuario logueado correctamente"
+        })
+    }
+}
