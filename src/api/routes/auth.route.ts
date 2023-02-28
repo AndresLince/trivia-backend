134d4566af6ea895d0b163dab1d6aecba9124dbf
AndresLince <lynxdarmatron@gmail.com>
2023-02-28 07:59:02 -0500

    Add auth route

---
+++ src/api/routes/auth.route.ts
@@ -0,0 +1,26 @@
+import { AuthHandler } from "../handlers/auth.handler";
+import { HttpUtilsHandler } from "../handlers/httpUtilsHandler";
+
+/*
+Route: /api/auth
+*/
+import { Router } from 'express';
+const router = Router();
+import { check } from 'express-validator';
+
+export class AuthRoute {
+    private authHandler: AuthHandler;
+    private httpUtilsHandler: HttpUtilsHandler;
+    constructor() {
+        this.authHandler = new AuthHandler();
+        this.httpUtilsHandler = new HttpUtilsHandler();
+    }
+
+    createRoutes(): Router {
+        router.post('/signup', [
+            check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
+            this.httpUtilsHandler.validateFields
+        ], this.authHandler.signUp);
+        return router
+    }
+}
