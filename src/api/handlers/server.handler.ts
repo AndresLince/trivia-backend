1c0bd48a634ba1d3c1598d3f207a7526667c9df2
AndresLince <lynxdarmatron@gmail.com>
2023-02-28 07:59:38 -0500

    Add auth route to the server handler

--- src/api/handlers/server.handler.ts
+++ src/api/handlers/server.handler.ts
@@ -1,11 +1,18 @@
 import express, { Express } from 'express';
+import {AuthRoute} from '../routes/auth.route';
 const cors = require('cors')
 
 export class ServerHandler{
+    private authRoutes: AuthRoute;
+    constructor() {
+        this.authRoutes = new AuthRoute();
+    }
     createServer(): Express {
         const app: Express = express();
         app.use(cors());
         app.use(express.json());
+
+        app.use('/api/auth', this.authRoutes.createRoutes())
 
         return app;
     }
