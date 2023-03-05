bf09334e019712abc24cb8ef29fddb5c30cca687
AndresLince <lynxdarmatron@gmail.com>
2023-03-05 16:20:05 -0500

    WIP index ts app configuration

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
-import  { ServerHandler }  from './src/api/handlers/server.handler';
-const serverHandler = new ServerHandler();
+import { DatabaseMysqlHandler } from './src/api/handlers/database/database.mysql.handler';
+import { ServerHandler } from './src/api/handlers/server.handler';
+import { UserRepositoryMysql } from './src/api/repositories/user.repository.mysql';
+import { ConfigService } from './src/api/services/config.service';
+let configService = new ConfigService();
+const databaseHandler = new DatabaseMysqlHandler({
+    configService
+});
+const userRepository = new UserRepositoryMysql({
+    databaseMysqlHandler: databaseHandler,
+});
+const serverHandler = new ServerHandler({
+    configService,
+    databaseHandler,
+    userRepository
+});
 const app = serverHandler.createServer();
-import dotenv from 'dotenv';
 
-dotenv.config();
 
-const port = process.env.PORT;
+
+const port = configService.getConfig('PORT');
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
