d3ad37b1be69220d411062b7b444c6f00ab5d733
AndresLince <lynxdarmatron@gmail.com>
2023-03-05 16:18:35 -0500

    WIP signUp service

--- src/api/handlers/auth.handler.ts
+++ src/api/handlers/auth.handler.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from 'express';
+import { AuthHandlerConstructorInterface } from '../interfaces/auth.handler.interface';
+import { CreateUserModel } from '../interfaces/createUser.model';
+import { UserRepositoryInterface } from '../interfaces/user.repository.interface';
 
 export class AuthHandler {
+    private userRepository: UserRepositoryInterface;
+    constructor({ userRepository }: AuthHandlerConstructorInterface) {
+        this.userRepository = userRepository
+        this.signUp = this.signUp.bind(this)
+    }
     signUp(request: Request, response: Response): void {
+        const userModel: CreateUserModel = {
+            username: "username",
+            ip: "ip",
+        }
+        let createdUser = this.userRepository.createUser(userModel);
         response.status(200).send({
             message: "Usuario logueado correctamente"
         });
