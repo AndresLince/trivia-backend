9e6737df824cfb83e601ee1679f7265ca7b288c3
AndresLince <lynxdarmatron@gmail.com>
2023-03-06 20:35:02 -0500

    Remove database handler constructor interface form server handler

--- src/api/interfaces/server.handler.interface.ts
+++ src/api/interfaces/server.handler.interface.ts
@@ -1,9 +1,5 @@
 import { AuthHandlerInterface } from "./auth.handler.interface";
 
-export interface DatabaseHandlerConstructorInterface {
-    getPool: Function;
-}
-
 export interface ServerHandlerConstructorInterface {
     authHandler: AuthHandlerInterface;
 }
