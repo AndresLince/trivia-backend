dcc26994ea6b78dad4004e3e8bd7f8acb999adce
AndresLince <lynxdarmatron@gmail.com>
2023-03-07 20:44:26 -0500

    user repository create user WIP

--- src/api/repositories/user.repository.mysql.ts
+++ src/api/repositories/user.repository.mysql.ts
@@ -7,9 +7,18 @@ export class UserRepositoryMysql implements UserRepositoryInterface {
     constructor({ databaseMysqlHandler }: UserRepositoryConstructorInterface) {
         this.databaseHandler = databaseMysqlHandler;
     }
-    async createUser(userCreateModel: CreateUserModel) {
-        const sql = `call createUser(?, ?, ?, ?, ?, ?, @last_id)`;
-        this.databaseHandler.getPool().query(sql, userCreateModel.username);
+    async createUser({ username, ip}: CreateUserModel) {
+        const sql = `call createUser(?,?, @last_id)`;
+        //const algo = this.databaseHandler.getPool().query(sql, username, ip);
+        const insertError = {error: false}
+        this.databaseHandler.getPool().query(sql, [username, ip], (error: Error, results: any, fields: any) => {
+            if (error) {
+                console.log("error",error)
+                insertError.error = true;
+            }
+        });
+
+        console.log("pasa", insertError);
         const sql2 = `SELECT @last_id AS insertId`;
         return this.databaseHandler.getPool().query(sql2);
     }
