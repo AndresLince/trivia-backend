2ced63a8701b0ac92018c2395364d36d6778c305
AndresLince <lynxdarmatron@gmail.com>
2023-03-16 06:44:16 -0500

    Create user model

--- src/api/interfaces/crud.responses.interface.ts
+++ src/api/interfaces/crud.responses.interface.ts
@@ -2,3 +2,10 @@ export interface InsertModel {
     username: string;
     ip: string;
 }
+
+export interface UserModel {
+    userName: string;
+    ip: string;
+    state: number;
+    idUser: number;
+}
