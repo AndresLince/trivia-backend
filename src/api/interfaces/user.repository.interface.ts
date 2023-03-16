c73a2d67e4e2016af330d84aa0af43d594b2ef1c
AndresLince <lynxdarmatron@gmail.com>
2023-03-16 06:43:41 -0500

    Add serach user by name to the user repository

--- src/api/interfaces/user.repository.interface.ts
+++ src/api/interfaces/user.repository.interface.ts
@@ -2,6 +2,7 @@ import { DatabaseHandlerInterface } from "./database.handler";
 
 export interface UserRepositoryInterface {
     createUser: Function;
+    searchUserByName: Function;
 }
 
 export interface UserRepositoryConstructorInterface {
