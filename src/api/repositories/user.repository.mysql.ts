41430ee2f22f70c9045b9825e26a39fb03edc086
AndresLince <lynxdarmatron@gmail.com>
2023-03-16 06:44:57 -0500

    Add search user by name to the user repository mysql

--- src/api/repositories/user.repository.mysql.ts
+++ src/api/repositories/user.repository.mysql.ts
@@ -1,5 +1,5 @@
 import { CreateUserModel } from '../interfaces/createUser.model';
-import { InsertModel } from '../interfaces/crud.responses.interface';
+import { InsertModel, UserModel } from '../interfaces/crud.responses.interface';
 import { DatabaseHandlerInterface } from '../interfaces/database.handler';
 import { UserRepositoryConstructorInterface, UserRepositoryInterface } from '../interfaces/user.repository.interface';
 
@@ -15,5 +15,24 @@ export class UserRepositoryMysql implements UserRepositoryInterface {
         const sql2 = `SELECT @last_id AS insertId`;
         const result = await this.databaseHandler.getPool().query(sql2);
         return result[ 0 ];
+    }
+    async searchUserByName({ username }: CreateUserModel): Promise<UserModel | null> {
+        const sql = `call searchUserByName(?)`;
+        const result = await this.databaseHandler.getPool().query(sql, [username]);
+
+        if (result[0].length === 0) {
+            return null;
+        }
+        let user: UserModel;
+
+        const { idUser, ip, userName, state } = result[0][0];
+        user = {
+            userName: userName,
+            ip: ip,
+            state: state,
+            idUser: idUser
+        };
+
+        return user;
     }
 }
