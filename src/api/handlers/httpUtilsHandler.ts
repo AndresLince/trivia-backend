a5e4c103ea462964106d79225e45aad8e1b45adc
AndresLince <lynxdarmatron@gmail.com>
2023-03-19 07:10:54 -0500

    Add generateJsonWebToken function to httpUtilsHandler

--- src/api/handlers/httpUtilsHandler.ts
+++ src/api/handlers/httpUtilsHandler.ts
@@ -1,7 +1,13 @@
 import { validationResult } from 'express-validator';
 import { Request, Response, NextFunction } from 'express';
+import { sign } from 'jsonwebtoken';
+import { ConfigService } from '../services/config.service';
 
 export class HttpUtilsHandler {
+    private configService: ConfigService;
+    constructor({ configService }: any) {
+        this.configService = configService;
+    }
     validateFields(request: Request, response: Response, next: NextFunction) {
         const errors = validationResult(request);
         if (!errors.isEmpty()) {
@@ -18,4 +24,20 @@ export class HttpUtilsHandler {
             message: message,
         });
     }
+    generateJsonWebToken = (modelId: string) => {
+        return new Promise((resolve, reject) => {
+            const payload = {
+                modelId
+            };
+            sign(payload, this.configService.getConfig('JSON_WEB_TOKEN_SECRET'), {
+                expiresIn: '12h'
+            }, (err, token) => {
+                if (err) {
+                    reject('No se pudo generar el JWT');
+                } else {
+                    resolve(token);
+                }
+            });
+        });
+    };
 }
