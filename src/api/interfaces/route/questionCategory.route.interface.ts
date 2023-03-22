89ba6be73dbce9f5be05742b5940691774ddf1b9
AndresLince <lynxdarmatron@gmail.com>
2023-03-22 07:44:22 -0500

    Add question category route interface

---
+++ src/api/interfaces/route/questionCategory.route.interface.ts
@@ -0,0 +1,5 @@
+import { QuestionCategoryHandlerInterface } from "../questionCategory.handler.interface";
+
+export interface QuestionCategoryRouteConstructorInterface {
+    questionCategoryHandler: QuestionCategoryHandlerInterface;
+}
