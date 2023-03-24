c1daffff7c5fdcfdb272985890a457397c2e2a4a
AndresLince <lynxdarmatron@gmail.com>
2023-03-24 05:41:22 -0500

    Add validate json webtoken to question category get all

--- src/api/routes/questionCategory.route.ts
+++ src/api/routes/questionCategory.route.ts
@@ -2,17 +2,22 @@
 Route: /api/question-category
 */
 import { Router } from 'express';
+import { HttpUtilsHandler } from '../handlers/httpUtilsHandler';
 const router = Router();
 import { QuestionCategoryHandlerInterface } from '../interfaces/questionCategory.handler.interface';
 import { QuestionCategoryRouteConstructorInterface } from '../interfaces/route/questionCategory.route.interface';
 
 export class QuestionCategoryRoute {
     private questionCategoryHandler: QuestionCategoryHandlerInterface;
-    constructor({ questionCategoryHandler }: QuestionCategoryRouteConstructorInterface) {
+    private httpUtilsHandler: HttpUtilsHandler;
+    constructor({ questionCategoryHandler, httpUtilsHandler }: QuestionCategoryRouteConstructorInterface) {
         this.questionCategoryHandler = questionCategoryHandler;
+        this.httpUtilsHandler = httpUtilsHandler
     }
 
     createRoutes(): Router {
-        return router.get('/', [], this.questionCategoryHandler.getAll);
+        return router.get('/', [
+            this.httpUtilsHandler.validateJsonWebToken
+        ], this.questionCategoryHandler.getAll);
     }
 }
