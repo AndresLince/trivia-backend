b891cbb3ce1df787fb93c38f87c7922e58b9d4d8
AndresLince <lynxdarmatron@gmail.com>
2023-03-24 05:40:39 -0500

    Add http utils handler to question category route interface

--- src/api/interfaces/route/questionCategory.route.interface.ts
+++ src/api/interfaces/route/questionCategory.route.interface.ts
@@ -1,5 +1,7 @@
+import { HttpUtilsHandler } from "../../handlers/httpUtilsHandler";
 import { QuestionCategoryHandlerInterface } from "../questionCategory.handler.interface";
 
 export interface QuestionCategoryRouteConstructorInterface {
     questionCategoryHandler: QuestionCategoryHandlerInterface;
+    httpUtilsHandler: HttpUtilsHandler
 }
