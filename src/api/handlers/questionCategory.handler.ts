b1f95677908ba7ecc9b1df5c7000a1780481af08
AndresLince <lynxdarmatron@gmail.com>
2023-03-24 06:00:06 -0500

    Add search All call to question category get all function

--- src/api/handlers/questionCategory.handler.ts
+++ src/api/handlers/questionCategory.handler.ts
@@ -12,8 +12,16 @@ export class QuestionCategoryHandler implements QuestionCategoryHandlerInterface
         this.getAll = this.getAll.bind(this);
     }
     async getAll(request: Request, response: Response): Promise<any> {
-        return response.status(200).send({
-            data: [],
-        });
+        try {
+            const questionCategories = await this.questionCategoryRepository.searchAll();
+            return response.status(200).send({
+                data: questionCategories,
+            });
+        } catch (error) {
+            console.log(error);
+            return response.status(500).send({
+                message: "Error interno por favor intenta nuevamente",
+            });
+        }
     }
 }
