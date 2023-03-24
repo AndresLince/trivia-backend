8304856cd9ceb52c7d45c531de54bd6d1cd5da5d
AndresLince <lynxdarmatron@gmail.com>
2023-03-23 19:52:27 -0500

    Configure question category handler

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { AuthHandler } from './src/api/handlers/auth.handler';
 import { DatabaseMysqlHandler } from './src/api/handlers/database/database.mysql.handler';
 import { HttpUtilsHandler } from './src/api/handlers/httpUtilsHandler';
+import { QuestionCategoryHandler } from './src/api/handlers/questionCategory.handler';
 import { ServerHandler } from './src/api/handlers/server.handler';
+import { QuestionCategoryRepositoryMysql } from './src/api/repositories/questionCategory.repository.mysql';
 import { UserRepositoryMysql } from './src/api/repositories/user.repository.mysql';
 import { ConfigService } from './src/api/services/config.service';
 const configService = new ConfigService();
@@ -11,6 +13,9 @@ const databaseHandler = new DatabaseMysqlHandler({
 const userRepository = new UserRepositoryMysql({
     databaseMysqlHandler: databaseHandler,
 });
+const questionCategoryRepository = new QuestionCategoryRepositoryMysql({
+    databaseHandler: databaseHandler,
+});
 const httpUtilsHandler = new HttpUtilsHandler({
     configService
 })
@@ -18,9 +23,14 @@ const authHandler = new AuthHandler({
     userRepository: userRepository,
     httpUtilsHandler: httpUtilsHandler,
 });
+const questionCategoryHandler = new QuestionCategoryHandler({
+    questionCategoryRepository: questionCategoryRepository,
+    httpUtilsHandler: httpUtilsHandler,
+});
 const serverHandler = new ServerHandler({
     authHandler,
     httpUtilsHandler,
+    questionCategoryHandler
 });
 const app = serverHandler.createServer();
 
