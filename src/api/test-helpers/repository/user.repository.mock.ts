daf71831ea643533f159de2924288281efd4d87e
AndresLince <lynxdarmatron@gmail.com>
2023-03-29 06:18:32 -0500

    Add user repository mock functionality

--- src/api/test-helpers/repository/user.repository.mock.ts
+++ src/api/test-helpers/repository/user.repository.mock.ts
@@ -1,9 +1,21 @@
 import { UserRepositoryInterface } from "../../interfaces/repository/user.repository.interface"
 
 const searchUserByName = jest.fn(userModel => {
-   return [];
+    const user = {
+        userName: userModel.userName,
+        ip: userModel.ip,
+        state: 1,
+        idUser: 1
+    };
+    if (userModel.userName === 'userNameDifferentIp') {
+        user.ip = '192.168.0.1';
+    }
+    if (userModel.userName === 'newUserName') {
+        return null;
+    }
+    return user;
 })
-const createUser = jest.fn(params => {
+const createUser = jest.fn(userModel => {
     return [];
 })
 const userRepositoryMock: UserRepositoryInterface = {
