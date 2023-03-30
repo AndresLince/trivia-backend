6a49cdaa83e0bfd2e6a85fbbfa875a642dd53508
AndresLince <lynxdarmatron@gmail.com>
2023-03-30 06:41:49 -0500

    Implement search function in trivia repository mysql

--- src/api/repositories/trivia.repository.mysql.ts
+++ src/api/repositories/trivia.repository.mysql.ts
@@ -1,6 +1,7 @@
 import { InsertModel } from '../interfaces/crud.responses.interface';
 import { DatabaseHandlerInterface } from '../interfaces/database.handler';
 import { CreateTrivia } from '../interfaces/model/create-trivia.model';
+import { TriviaModel } from '../interfaces/model/trivia.model';
 import { TriviaConstructorInterface, TriviaRepositoryInterface } from '../interfaces/repository/trivia.repository.interface';
 
 export class TriviaRepositoryMysql implements TriviaRepositoryInterface {
@@ -15,5 +16,23 @@ export class TriviaRepositoryMysql implements TriviaRepositoryInterface {
         const sql2 = `SELECT @last_id AS insertId`;
         const result = await this.databaseHandler.getPool().query(sql2);
         return result[0];
+    }
+    async search(idUser: string): Promise<TriviaModel | null> {
+        const sql = `call searchTrivia(?)`;
+        const result = await this.databaseHandler.getPool().query(sql, [idUser]);
+
+        if (result[0].length === 0) {
+            return null;
+        }
+        let trivia: TriviaModel;
+
+        const triviaDb = result[0][0];
+        trivia = {
+            idTrivia: triviaDb.idTrivia,
+            idUser: triviaDb.idUser,
+            idQuestionCategory: triviaDb.idQuestionCategory
+        };
+
+        return trivia;
     }
 }
