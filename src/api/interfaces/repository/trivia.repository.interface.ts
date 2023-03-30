6fa9ad06d99d1c24258b4a01592f183329ce48ea
AndresLince <lynxdarmatron@gmail.com>
2023-03-30 06:41:07 -0500

    Add search function to trivia repository interface

--- src/api/interfaces/repository/trivia.repository.interface.ts
+++ src/api/interfaces/repository/trivia.repository.interface.ts
@@ -2,6 +2,7 @@ import { DatabaseHandlerInterface } from "../database.handler";
 
 export interface TriviaRepositoryInterface {
     create: Function;
+    search: Function;
 }
 
 export interface TriviaConstructorInterface{
