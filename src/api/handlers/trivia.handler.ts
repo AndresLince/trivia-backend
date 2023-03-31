903a647567a918ffda4508d7bac33f39f85a446b
AndresLince <lynxdarmatron@gmail.com>
2023-03-31 06:00:00 -0500

    Add search trivia to create function on trivia repository

--- src/api/handlers/trivia.handler.ts
+++ src/api/handlers/trivia.handler.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { CryptoHandlerInterface } from '../interfaces/handler/crypto.handler.interface';
 import { TriviaHandlerConstructorInterface, TriviaHandlerInterface } from '../interfaces/handler/trivia.handler.interface';
 import { CreateTrivia } from '../interfaces/model/create-trivia.model';
 import { TriviaRepositoryInterface } from '../interfaces/repository/trivia.repository.interface';
@@ -7,9 +8,11 @@ import { HttpUtilsHandler } from './httpUtilsHandler';
 export class TriviaHandler implements TriviaHandlerInterface {
     private triviaRepository: TriviaRepositoryInterface;
     private httpUtilsHandler: HttpUtilsHandler;
-    constructor({ triviaRepository, httpUtilsHandler }: TriviaHandlerConstructorInterface) {
+    private cryptoHandler: CryptoHandlerInterface
+    constructor({ triviaRepository, httpUtilsHandler, cryptoHandler }: TriviaHandlerConstructorInterface) {
         this.triviaRepository = triviaRepository;
         this.httpUtilsHandler = httpUtilsHandler;
+        this.cryptoHandler = cryptoHandler;
         this.create = this.create.bind(this);
     }
     async create(request: Request, response: Response): Promise<any> {
@@ -21,9 +24,18 @@ export class TriviaHandler implements TriviaHandlerInterface {
         };
 
         try {
+            const trivia = await this.triviaRepository.search(triviaModel.idUser);
+            if (trivia) {
+                return response.status(200).send({
+                    idTrivia: this.cryptoHandler.encrypt(trivia.idTrivia.toString()),
+                });
+            }
+
             const { insertId } = await this.triviaRepository.create(triviaModel);
 
-            return this.httpUtilsHandler.sendBasicJsonResponse(response, 200, "trivia creada correctamente");
+            return response.status(200).send({
+                idTrivia: this.cryptoHandler.encrypt(insertId.toString()),
+            });
         } catch (error) {
             console.log(error);
             return this.httpUtilsHandler.sendBasicJsonResponse(response, 500, "Error interno por favor intenta nuevamente");
