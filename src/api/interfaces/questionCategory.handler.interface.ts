d319e9e7adc169247778d7c0682aa938a1f36aee
AndresLince <lynxdarmatron@gmail.com>
2023-04-01 20:51:19 -0500

    Add http utils handler interface to question category handler interface

--- src/api/interfaces/questionCategory.handler.interface.ts
+++ src/api/interfaces/questionCategory.handler.interface.ts
@@ -1,11 +1,11 @@
 import { RequestHandler } from "express";
-import { HttpUtilsHandler } from "../handlers/httpUtilsHandler";
 import { CryptoHandlerInterface } from "./handler/crypto.handler.interface";
+import { HttpUtilsHandlerInterface } from "./handler/http.handler.interface";
 import { QuestionCategoryRepositoryInterface } from "./questionCategory.repository.interface";
 
 export interface QuestionCategoryHandlerConstructorInterface {
     questionCategoryRepository: QuestionCategoryRepositoryInterface;
-    httpUtilsHandler: HttpUtilsHandler;
+    httpUtilsHandler: HttpUtilsHandlerInterface;
     cryptoHandler: CryptoHandlerInterface
 }
 
