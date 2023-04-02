a187e911806a9c9d79667069f1ac286cf09ea2bb
AndresLince <lynxdarmatron@gmail.com>
2023-04-01 20:40:25 -0500

    Add http handler interface

---
+++ src/api/interfaces/handler/http.handler.interface.ts
@@ -0,0 +1,13 @@
+import { ErrorRequestHandler, RequestHandler } from "express";
+import { ConfigService } from "../../services/config.service";
+
+export interface HttpUtilsHandlerConstructorInterface {
+    configService: ConfigService;
+}
+
+export interface HttpUtilsHandlerInterface {
+    validateFields: RequestHandler;
+    sendBasicJsonResponse: Function;
+    generateJsonWebToken: Function;
+    validateJsonWebToken: RequestHandler;
+}
