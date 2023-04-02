e0237cbca53686218cd1e51b019c2515aee99c44
AndresLince <lynxdarmatron@gmail.com>
2023-04-01 20:48:54 -0500

    Add http utils handler interface to server handler

--- src/api/interfaces/server.handler.interface.ts
+++ src/api/interfaces/server.handler.interface.ts
@@ -1,11 +1,11 @@
-import { HttpUtilsHandler } from "../handlers/httpUtilsHandler";
 import { AuthHandlerInterface } from "./auth.handler.interface";
+import { HttpUtilsHandlerInterface } from "./handler/http.handler.interface";
 import { QuestionCategoryHandlerInterface } from "./questionCategory.handler.interface";
 import { RouteInterface } from "./route/route.interface";
 
 export interface ServerHandlerConstructorInterface {
     authHandler: AuthHandlerInterface;
-    httpUtilsHandler: HttpUtilsHandler;
+    httpUtilsHandler: HttpUtilsHandlerInterface;
     questionCategoryHandler: QuestionCategoryHandlerInterface;
     triviaRoute: RouteInterface;
 }
