0efa05464bb7d0b1c8b8c9d9064140614abca700
AndresLince <lynxdarmatron@gmail.com>
2023-04-01 20:46:52 -0500

    Add http utils handler interface to trivia route

--- src/api/routes/trivia.route.ts
+++ src/api/routes/trivia.route.ts
@@ -2,16 +2,16 @@
 Route: /api/trivia
 */
 import { Router } from 'express';
-import { HttpUtilsHandler } from '../handlers/httpUtilsHandler';
 import { TriviaHandlerInterface } from '../interfaces/handler/trivia.handler.interface';
 import { check } from 'express-validator';
 const router = Router();
 import { TriviaRouteConstructorInterface } from '../interfaces/route/trivia.route.interface';
 import { RouteInterface } from '../interfaces/route/route.interface';
+import { HttpUtilsHandlerInterface } from '../interfaces/handler/http.handler.interface';
 
 export class TriviaRoute implements RouteInterface {
     private triviaHandler: TriviaHandlerInterface;
-    private httpUtilsHandler: HttpUtilsHandler;
+    private httpUtilsHandler: HttpUtilsHandlerInterface;
     constructor({ triviaHandler, httpUtilsHandler }: TriviaRouteConstructorInterface) {
         this.triviaHandler = triviaHandler;
         this.httpUtilsHandler = httpUtilsHandler
