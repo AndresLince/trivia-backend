e8e85d777e6e79d79b69d6093f6b3962763d0775
AndresLince <lynxdarmatron@gmail.com>
2023-04-26 06:13:04 -0500

    Add question categories to the search all function on question category repository mock

--- src/api/test-helpers/repository/question-category.repository.mock.ts
+++ src/api/test-helpers/repository/question-category.repository.mock.ts
@@ -1,7 +1,11 @@
 import { QuestionCategoryRepositoryInterface } from "../../interfaces/questionCategory.repository.interface";
 
 const searchAll = jest.fn(questionCategoryModel => {
-   return [];
+   return [
+       { idQuestionCategory: 1, name: 'Category 1' },
+       { idQuestionCategory: 2, name: 'Category 2' },
+       { idQuestionCategory: 3, name: 'Category 3' },
+   ];
 });
 const questionCategoryRepository: QuestionCategoryRepositoryInterface = {
     searchAll
