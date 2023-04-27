4a7cf97e33d862ebf44de4fc7b1638cb0b5a4a66
AndresLince <lynxdarmatron@gmail.com>
2023-04-27 05:59:15 -0500

    Add create trivia test

--- src/api/handlers/__tests__/trivia.handler.ts
+++ src/api/handlers/__tests__/trivia.handler.ts
@@ -6,4 +6,19 @@ describe('Trivia handler create tests', () => {
     it('Should return 401 Unauthorized', () => {
         return request(app).post('/api/trivia').expect(401);
     });
+    it('Should return 200 Successful', () => {
+        return request(app).post('/api/trivia').set(
+            { 'x-token': 'mytokennewuser' }
+        ).send(
+            { 'idQuestionCategory': '1'}
+        )
+        .then((response: Response) => {
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    idTrivia: expect.any(String),
+                })
+            ),
+            expect(response.status).toBe(200);
+        });
+    });
 });
