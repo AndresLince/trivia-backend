3552537df64ee0b3ab24df8a3124c7b68a98103c
AndresLince <lynxdarmatron@gmail.com>
2023-04-29 11:06:40 -0500

    Add question data test helper

---
+++ src/api/test-helpers/data/question.data.ts
@@ -0,0 +1,22 @@
+export const questionDataMock = {
+    idQuestion: 1,
+    description: 'First question',
+    answers: [
+        {
+            idAnswer: 1,
+            description: 'First answer'
+        },
+        {
+            idAnswer: 2,
+            description: 'Second answer'
+        },
+        {
+            idAnswer: 3,
+            description: 'Third answer'
+        },
+        {
+            idAnswer: 4,
+            description: 'Four answer'
+        },
+    ]
+};
