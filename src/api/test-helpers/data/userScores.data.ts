00a48d8850e70794274bf3693036300934c90e18
AndresLince <lynxdarmatron@gmail.com>
2023-05-01 11:54:59 -0500

    Add userScores data

---
+++ src/api/test-helpers/data/userScores.data.ts
@@ -0,0 +1,12 @@
+export const userScoresMock = [
+    {
+        idUser: '1',
+        userName: "user1",
+        score: "500"
+    },
+    {
+        idUser: '2',
+        userName: "user2",
+        score: "502"
+    }
+];
