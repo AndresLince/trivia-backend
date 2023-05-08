345916cbdd571282eeb9419fc9008ea2293e4366
AndresLince <lynxdarmatron@gmail.com>
2023-05-08 06:57:28 -0500

    Improve question model

--- src/api/test-helpers/data/question.data.ts
+++ src/api/test-helpers/data/question.data.ts
@@ -1,21 +1,23 @@
-export const questionDataMock = {
-    idQuestion: 1,
+import { Question } from "../../interfaces/model/question.model";
+
+export const questionDataMock: Question = {
+    idQuestion: '1',
     description: 'First question',
     answers: [
         {
-            idAnswer: 1,
+            idAnswer: '1',
             description: 'First answer'
         },
         {
-            idAnswer: 2,
+            idAnswer: '2',
             description: 'Second answer'
         },
         {
-            idAnswer: 3,
+            idAnswer: '3',
             description: 'Third answer'
         },
         {
-            idAnswer: 4,
+            idAnswer: '4',
             description: 'Four answer'
         },
     ]
