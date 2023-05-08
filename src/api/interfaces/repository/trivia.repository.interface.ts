e6ad85c55b16b3fdcc4fd3820d2863860e46deed
AndresLince <lynxdarmatron@gmail.com>
2023-05-08 06:58:00 -0500

    Improve trivia repository interface

--- src/api/interfaces/repository/trivia.repository.interface.ts
+++ src/api/interfaces/repository/trivia.repository.interface.ts
@@ -2,13 +2,14 @@ import { InsertModel } from "../crud.responses.interface";
 import { DatabaseHandlerInterface } from "../database.handler";
 import { AddQuestionsToTrivia } from "../model/add-question-to-trivia.model";
 import { CreateTrivia } from "../model/create-trivia.model";
+import { Question } from "../model/question.model";
 import { TriviaModel } from "../model/trivia.model";
 
 export interface TriviaRepositoryInterface {
     create(createTrivia: CreateTrivia): Promise<InsertModel>;
     addQuestionsToTrivia(addQuestionsToTrivia: AddQuestionsToTrivia): Promise<boolean>;
     search(idUser: string): Promise<TriviaModel | null>;
-    getQuestion: Function;
+    getQuestion(idTrivia: string): Promise<Question | null>;
     setSelectedAnswer: Function;
     getScore: Function;
     closeTrivia: Function;
