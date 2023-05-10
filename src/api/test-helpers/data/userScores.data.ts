cc8aaf197d4de536689d5fec0b61cea42f274083
AndresLince <lynxdarmatron@gmail.com>
2023-05-10 05:56:11 -0500

    Fix userScores data

--- src/api/test-helpers/data/userScores.data.ts
+++ src/api/test-helpers/data/userScores.data.ts
@@ -1,12 +1,14 @@
-export const userScoresMock = [
+import { UserScore } from "../../interfaces/model/user-score.model";
+
+export const userScoresMock: Array<UserScore> = [
     {
         idUser: '1',
         userName: "user1",
-        score: "500"
+        score: 500
     },
     {
         idUser: '2',
         userName: "user2",
-        score: "502"
+        score: 502
     }
 ];
