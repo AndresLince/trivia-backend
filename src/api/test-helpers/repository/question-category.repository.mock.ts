c2d73457c6df3c4a030e39822964efa8960fef2a
AndresLince <lynxdarmatron@gmail.com>
2023-05-16 06:29:07 -0500

    Fix linter errors

--- src/api/test-helpers/repository/question-category.repository.mock.ts
+++ src/api/test-helpers/repository/question-category.repository.mock.ts
@@ -1,11 +1,13 @@
+import { QuestionCategory } from "../../interfaces/model/question-category.model";
 import { QuestionCategoryRepositoryInterface } from "../../interfaces/questionCategory.repository.interface";
 
 const searchAll = jest.fn(() => {
-   return [
-       { idQuestionCategory: 1, name: 'Category 1' },
-       { idQuestionCategory: 2, name: 'Category 2' },
-       { idQuestionCategory: 3, name: 'Category 3' },
-   ];
+    const questionCategory: QuestionCategory[] = [
+        { idQuestionCategory: '1', name: 'Category 1' },
+        { idQuestionCategory: '2', name: 'Category 2' },
+        { idQuestionCategory: '3', name: 'Category 3' },
+    ]
+    return Promise.resolve(questionCategory);
 });
 const questionCategoryRepository: QuestionCategoryRepositoryInterface = {
     searchAll
