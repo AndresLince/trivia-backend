dc50a52ed943ec2515fea826806e12f1e9f263d0
AndresLince <lynxdarmatron@gmail.com>
2023-05-18 07:21:50 -0500

    Fix linter errors

--- src/api/handlers/__tests__/trivia.handler.ts
+++ src/api/handlers/__tests__/trivia.handler.ts
@@ -24,29 +24,28 @@ describe('Trivia handler create tests', () => {
         );
         expect(response.status).toBe(200);
     });
-    it('Should return 200 Successful', () => {
-        return request(app).post('/api/trivia').set(
+    it('Should return 200 Successful', async () => {
+        const response = await request(app).post('/api/trivia').set(
             { 'x-token': 'mytokenuserwithouttrivia' }
         ).send(
             { 'idQuestionCategory': '1' }
-        )
-        .then((response: Response) => {
-            expect(response.body).toEqual(
-                expect.objectContaining({
-                    idTrivia: expect.any(String),
-                })
-            ),
-            expect(response.status).toBe(200);
-        });
+        );
+
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                idTrivia: expect.any(String),
+            })
+        );
+        expect(response.status).toBe(200);
     });
-    it('Should return 500 Internal server error', () => {
-        return request(app).post('/api/trivia').set(
+    it('Should return 500 Internal server error', async() => {
+        const response = await request(app).post('/api/trivia').set(
             { 'x-token': 'exceptionToken' }
         ).send(
             { 'idQuestionCategory': '1' }
-        ).then((response: Response) => {
-            expect(response.status).toBe(500);
-        });
+        );
+
+        expect(response.status).toBe(500);
     });
     it('Should return 400 Bad Request', () => {
         return request(app).post('/api/trivia').set(
